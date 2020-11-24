54207fe9c128f09224a7574ffc1aef69911081c9
Danilo de Canini <danilodecanini@gmail.com>
2020-11-23 21:40:24 -0300

    🚀 feat: add SignIn and SignUp pages w/ components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-function App() {
-  return (
-    <h1>GoBarber</h1>
-  );
-}
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyles />
+  </>
+);
 
 export default App;
