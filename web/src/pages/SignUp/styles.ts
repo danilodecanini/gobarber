10a717e118dfa2928c986bcf4f93ee392733598b
Danilo de Canini <danilodecanini@gmail.com>
2020-11-25 21:30:04 -0300

    🚀 feat: setup Routes

--- web/src/pages/SignUp/styles.ts
+++ web/src/pages/SignUp/styles.ts
@@ -1,5 +1,5 @@
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
-import styled from 'styled-components';
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
@@ -14,9 +14,31 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  place-content: center;
+  justify-content: center;
+
   width: 100%;
   max-width: 700px;
+`;
+
+const apperFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(+50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${apperFromRight} 1s;
+
   form {
     margin: 80px 0;
     width: 340px;
@@ -31,22 +53,6 @@ export const Content = styled.div`
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
-    &:hover {
-      color: ${shade(0.2, '#f4ede8')};
-    }
-  }
-
-  > a {
-    color: #f4ede8;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-    display: flex;
-    align-items: center;
-    svg {
-      margin-right: 16px;
-    }
     &:hover {
       color: ${shade(0.2, '#f4ede8')};
     }
