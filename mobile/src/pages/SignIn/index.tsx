2b964b83e72e12363fe02bb9fcd73cad18bc35dd
Danilo de Canini <danilodecanini@gmail.com>
2020-11-26 22:02:55 -0300

    🚀 feat: add Input and Button components w/ Icons

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -4,12 +4,25 @@ import { Image } from 'react-native';
 import { Container, Title } from './styles';
 
 import LogoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={LogoImg} />
       <Title>Faça o seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('pressionado!');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
