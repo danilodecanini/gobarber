a36e054d3091d457861c6abd496b174c1746bf93
Danilo de Canini <danilodecanini@gmail.com>
2020-11-26 21:21:22 -0300

    🚀 feat: add font family, auth routes w/ initial pages

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container, Title } from './styles';
+
+import LogoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={LogoImg} />
+      <Title>Faça o seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
