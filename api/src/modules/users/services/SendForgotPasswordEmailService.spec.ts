bda8125508ff2a3706a9891fd760c70080f03984
Danilo de Canini <danilodecanini@gmail.com>
2020-12-21 21:56:54 -0300

    🧰 test: add send forgot email test

---
+++ api/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEMail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+    });
+
+    await sendForgotPasswordEMail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toBeCalled();
+  });
+});
