7788d3e50d7490bb020129a7d0ca2f1e926487dc
Danilo de Canini <danilodecanini@gmail.com>
2020-12-23 22:17:03 -0300

    🚀 feat: send email forgot pwd and reset pwd

--- api/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ api/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -16,17 +16,21 @@ export default class EtherealMailProvider implements IMailProvider {
         },
       });
 
+      console.log(account);
+
       this.client = transporter;
     });
   }
 
   public async sendMail(to: string, body: string): Promise<void> {
-    this.client.sendMail({
-      from: 'Sender Name <sender@example.com>',
-      to: 'Recipient <recipient@example.com>',
-      subject: 'Nodemailer is unicode friendly ✔',
-      text: 'Hello to myself!',
-      html: '<p><b>Hello</b> to myself!</p>',
+    const message = await this.client.sendMail({
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperação de Senha',
+      text: body,
     });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   }
 }
