71990ac1f90b12c7b53477b61876c94a62a9a0e0
Danilo de Canini <danilodecanini@gmail.com>
2021-01-20 23:12:28 -0300

    🚀 feat: add ListProviderDayAvailability with tests

---
+++ api/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+
+let listProviderDayAvailabilty: ListProviderDayAvailabilityService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderDayAvailabilty = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able list the day availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 10, 0, 0),
+    });
+
+    const availability = await listProviderDayAvailabilty.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 5,
+      day: 21,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: true },
+        { hour: 10, available: false },
+        { hour: 11, available: true },
+      ]),
+    );
+  });
+});
--- api/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ api/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,4 +1,4 @@
-import { getDate, getDaysInMonth } from 'date-fns';
+import { getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -28,7 +28,33 @@ class ListProviderDayAvailabilityService {
     month,
     day,
   }: IRequest): Promise<IResponse> {
-    return [{ hour: 8, available: true }];
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+      return {
+        hour,
+        available: !hasAppointmentInHour,
+      };
+    });
+
+    return availability;
   }
 }
 
